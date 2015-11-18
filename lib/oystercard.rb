7aaeee2a7bbea608842392b876d456aeaa0340dc
Leo Allen <leo.f.allen@gmail.com>
2015-11-18 15:28:55 +0000

    #11 Saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,38 +1,54 @@
 class Oystercard
 
-  LIMIT = 90
+  MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
 
-  attr_reader :balance
+  attr_reader :balance, :current_journey, :journey_history
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @journey_history = []
   end
 
   def top_up(amount_received)
     self.balance += amount_received
-    raise "Over limit of £#{LIMIT}" if balance > LIMIT
+    raise "Over limit of £#{MAXIMUM_BALANCE}" if above_maximum_balance?
+  end
+
+  def in_journey?
+    !current_journey.nil?
+  end
+
+  def touch_in(journey)
+    raise "Too low funds" if below_minimum_balance?
+    self.current_journey = journey
+  end
+
+  def touch_out
+    self.journey_history << current_journey
+    self.current_journey = nil
+    deduct(journey_fare)
+  end
+
+  private
+
+  attr_writer :balance, :current_journey
+  attr_accessor :in_journey
+
+  def journey_fare
+    1
   end
 
   def deduct(amount_deducted)
     self.balance -= amount_deducted
   end
 
-  def in_journey?
-    in_journey
+  def below_minimum_balance?
+    balance < MINIMUM_BALANCE
   end
 
-  def touch_in
-    self.in_journey = true
+  def above_maximum_balance?
+    balance > MAXIMUM_BALANCE
   end
-
-  def touch_out
-    self.in_journey = false
-  end
-
-  private
-
-  attr_writer :balance
-  attr_accessor :in_journey
 
 end
